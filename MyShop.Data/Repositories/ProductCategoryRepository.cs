67d8972e7601d575de3c488881b1be2b601ef640
vovuong <vovuong1025@gmail.com>
2020-12-14 02:10:26 +0700

    commit migration and repository

---
+++ MyShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Data.Infrastructure;
+using MyShop.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyShop.Data.Repositories
+{
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
