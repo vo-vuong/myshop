7a5cbee3b2405f6ca3beb8e5e6017a7b66bc4562
vovuong <vovuong1025@gmail.com>
2020-12-22 22:49:17 +0700

    add dependency injection autofac to web

--- MyShop/API/PostCategoryController.cs
+++ MyShop/API/PostCategoryController.cs
@@ -24,18 +24,9 @@ namespace MyShop.Web.Api
         {
             return CreateHttpResponse(request, () =>
             {
-                HttpResponseMessage response = null;
-                if (ModelState.IsValid)
-                {
-                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                }
-                else
-                {
-                    var listCategory = _postCategoryService.GetAll();
+                var listCategory = _postCategoryService.GetAll();
 
-                    response = request.CreateResponse(HttpStatusCode.OK, listCategory);
-
-                }
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
                 return response;
             });
         }
---
+++ MyShop/App_Start/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+using System.Reflection;
+using MyShop.Data.Infrastructure;
+using MyShop.Data.Repositories;
+using MyShop.Service;
+using System.Web.Mvc;
+using System.Web.Http;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using MyShop.Data;
+[assembly: OwinStartup(typeof(MyShop.Web.App_Start.Startup))]
+
+namespace MyShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //Register WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<MyShopDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
+
+        }
+    }
+}
